93fb11c5445acced8b84fd745f5a84dbc2becd04
maxamarcus <maxamarcus@gmail.com>
2022-06-23 16:12:28 -0400

    Notes.

--- List.h
+++ List.h
@@ -1,6 +1,6 @@
 // List ABC
 
-// Members do not repeat. All list items have distinct key values.
+/* Every list element has a unique key value. This is enforced in the implementations. */
 
 template <class t>
 class List {
@@ -8,9 +8,9 @@ public:
 
     // CLASS METHODS
     virtual void Put(t) = 0;
-    // Implementation must not allow adding more than one of an item.
+    /* Implementation must not allow adding more than one of an item. */
     virtual int GetPositionOf(t) = 0;
-    // Return -1 (invalid value) if argument is not in list.
+    /* Return -1 (invalid value) if argument is not in list. */
     virtual bool IsInList(t) = 0;
     virtual void Remove(t) = 0;
     virtual int GetLength() = 0;
@@ -18,7 +18,7 @@ public:
 
     // ITERATORS
     virtual void ResetList() = 0;
-    // Set current position before first item.
+    /* Set current position before first item. Use this before iterating, so that GetNext() returns the first item. */
     virtual t GetNext() = 0; 
-    // Advance current position, return the item.
+    /* The "next" list item after the last list item is the first list item -- ie, list can iterate circularly. */
 };
